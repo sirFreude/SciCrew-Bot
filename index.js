4fafc5f6058e811985c4098fd4e2aaeb218e1ff3
sirFreude <97123014+sirFreude@users.noreply.github.com>
2022-01-04 20:12:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,33 +9,37 @@ const client = new DiscordJS.Client({
     ],
   })
 
-  // Listen for when the bot is ready
 client.on('ready', () => {
   console.log('The bot is ready!')
 })
-// Listen for new messages
 client.on('messageCreate', (msg) => {
-  // Was the message "ping"?
-  if (msg.content === 'coordinates.SciBot') {
-    // Reply with "pong"
+  if (msg.content === 'help.SciBot') {
     msg.reply({
-      content: '**SciBase:** x1800 z-2600 \n**SciDistrict:** x20000 z0 (**Ice Farm:** x20000 z2000 --- **Witch Farm:** x19500 z2700)',
+      content: '**SciBot Commands:** \n\n*help.SciBot:* will display this message. \n*info.SciBot:* will display information about the Minecraft Server and this Discord Bot. \n*coordinates:* will display coordinates of some areas.',
     })
   }
 })
 
-// Listen for when the bot is ready
 client.on('ready', () => {
   console.log('The bot is ready!')
 })
-// Listen for new messages
 client.on('messageCreate', (msg) => {
-  // Was the message "ping"?
+  if (msg.content === 'coordinates.SciBot') {
+    msg.reply({
+      content: '**SciBase:** x1800 z-2600 \n**SciDistrict:** x20000 z0 -> **Ice Farm:** x20000 z2000 --- **Witch Farm:** x19500 z2700)',
+    })
+  }
+})
+
+client.on('ready', () => {
+  console.log('The bot is ready!')
+})
+client.on('messageCreate', (msg) => {
   if (msg.content === 'server.SciBot') {
-    // Reply with "pong"
     msg.reply({
       content: '**About Server:** Paper version git-Paper-18 **(MC: 1.18)** (Implementing API version 1.18-R0.1-SNAPSHOT) (GIT: 13a445c) \n**Seed:** -1171947030733164536',
     })
   }
 })
   client.login(process.env.TOKEN)
+// DM me on discord for the TOKEN: sirFreude#6330
